35ceee68641b20f1f672c9ef5eda5a4442be1b2a
Eduardo Zaparoli <eduzzx1204@gmail.com>
2024-02-03 14:32:43 -0300

    folder restructuring

---
+++ frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { AuthStore } from '@/stores/AuthStore';
+import { Container } from '@/components/Container';
+import { LoginContainer } from '@/components/LoginContainer';
+
+const Home = () => {
+
+    const { fetchAccessToken } = new AuthStore()
+
+    const router = useRouter()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onContinue = async () => {
+        try {
+            await fetchAccessToken(email, password)
+            router.push('/dashboard')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            <Container align='center' justifyContent='center' navbar>
+                <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
+                    <LoginContainer onContinue={onContinue} email={email} password={password} onEmail={setEmail} onPassword={setPassword} />
+                </Flex>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,11 @@
 "use client"
-import { Flex } from '@chakra-ui/react';
-import { Container } from './components/Container';
-import { LoginContainer } from './components/LoginContainer';
-import { useState } from 'react';
-import { AuthStore } from './stores/AuthStore';
-import { useRouter } from 'next/navigation';
+import NotFound from './not-found';
 
 const Home = () => {
 
-  const { fetchAccessToken } = new AuthStore()
-
-  const router = useRouter()
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const onContinue = async () => {
-    try {
-      await fetchAccessToken(email, password)
-      router.push('invest/dashboard')
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
     <>
-      <Container align='center' justifyContent='center' navbar>
-        <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
-          <LoginContainer onContinue={onContinue} email={email} password={password} onEmail={setEmail} onPassword={setPassword} />
-        </Flex>
-      </Container>
+      <NotFound />
     </>
   );
 }
